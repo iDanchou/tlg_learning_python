49f15efc3bca9a1ccb62f0b029401fd39c286c04
iDanchou <rkirkland18@live.com>
2020-07-21 12:59:20 -0700

    moving everything to folders

---
+++ practice/listpractice.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+integers = [1, 2, 4, 5, 10] # a list of integers
+
+print(integers)
+
+integers.append(100) # add 100 to the end of the list
+
+print(integers)
+
+integers.append(6) # add 6 to the end of the list
+
+print(integers)
+
+integers.pop() # removes last value from the end of the list
+
+print(integers)
---
+++ practice/stringpractice.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# shift + F10 to run code
+
+lang = 'python'
+lang = lang.upper() # makes string uppercase
+print(lang)
+lang = lang.capitalize() # makes first letter capital
+print(lang)
+lang[2].upper()
