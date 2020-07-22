3de56155bcb52644b516d8eda3ea4806454dcf34
iDanchou <rkirkland18@live.com>
2020-07-22 14:40:53 -0700

    new examples and exercises

---
+++ lab_input/integerinput.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+
+# to read in an integer and double it
+
+user_input = int(input('Type in an integer ')) # converts user input to int
+double = user_input * 2  # multiply user input by 2
+print('You entered {}: Result doubled = {}'.format(user_input, double))
---
+++ lab_input/nameinput.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+# Python script that reads in user name and prints output
+
+name = input('What is your name? ')
+print('Your name is {}'.format(name))
