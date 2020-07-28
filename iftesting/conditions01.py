574f597e5fe989e279c46f115703ef12d160f48f
iDanchou <rkirkland18@live.com>
2020-07-28 17:00:20 -0400

    new if training

---
+++ iftesting/conditions01.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+hostname = input('What value should we set for the hostname: ')
+
+if hostname.lower() == 'mtg': # if conditions are met, these commands will run
+    print('The hostname was found to be mtg')
+    print('hostname matches expected config')
+
+print('Exiting the script') # indication of the closure of the script
