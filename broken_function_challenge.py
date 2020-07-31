700a67f7b1aee2eaa97e29dfab10254603585b60
iDanchou <rkirkland18@live.com>
2020-07-31 14:18:08 -0700

    new

--- broken_function_challenge.py
+++ broken_function_challenge.py
@@ -4,10 +4,10 @@ import random
 num = input("How many Shakespearean insults would you like? ")
 
 insult_list = open("/home/captain/tlg_learning_python/insults.txt", "r")
-
+insult = insult_list.readlines()
 def insult_generator(num):
     print("You are a", end="")
-    for x in num:
-      print(random.choice(insult_list))
+    for range in num:
+      print(random.choice(insult))
 
 insult_generator(num)
--- calculator.py
+++ calculator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def choose_calc():
-    option = str(input(f"""\nWould you like to use:
-    (A). Addition:
-    (B). Subtraction:
-    (C). Multiplication:
-    (D). Division
-Please enter an option:"""))
+    option = str(input("""\nWould you like to use:
+(A). Addition:
+(B). Subtraction:
+(C). Multiplication:
+(D). Division
+Please enter an option: """))
     if(option.lower() == "a"):
         addition()
     elif(option.lower() == "b"):
@@ -22,75 +22,75 @@ Please enter an option:"""))
 def addition():
     while True:
         try:
-            num1 = float(input(f"\nEnter your first number for Addition: "))
+            num1 = float(input("\nEnter your first number for Addition: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     while True:
         try:
-            num2 = float(input(f"Enter your second number for Addition: "))
+            num2 = float(input("Enter your second number for Addition: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     print(num1 + num2)
     go_again()
 
 def subtraction():
     while True:
         try:
-            num1 = float(input(f"\nEnter your first number for Subtractraction: "))
+            num1 = float(input("\nEnter your first number for Subtractraction: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     while True:
         try:
-            num2 = float(input(f"Enter your second number for  Subtractraction: "))
+            num2 = float(input("Enter your second number for  Subtractraction: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     print(num1 - num2)
     go_again()
 
 def multiplication():
     while True:
         try:
-            num1 = float(input(f"\nEnter your first number for Multiplication: "))
+            num1 = float(input("\nEnter your first number for Multiplication: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     while True:
         try:
-            num2 = float(input(f"Enter your second number for Multiplication: "))
+            num2 = float(input("Enter your second number for Multiplication: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     print(num1 * num2)
     go_again()
 
 def division():
     while True:
         try:
-            num1 = float(input(f"\nEnter the 1st number you wish to Divide: "))
+            num1 = float(input("\nEnter the 1st number you wish to Divide: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     while True:
         try:
-            num2 = float(input(f"Enter the 2nd number you wish to Divide: "))
+            num2 = float(input("Enter the 2nd number you wish to Divide: "))
             break
         except ValueError:
-            print(f"This was not valid input. Please try again.")
+            print("This was not valid input. Please try again.")
     print(num1/num2)
     go_again()
 
 def go_again():
-    again = str(input(f"Would you like to perform any other functions? Y or N: "))
-    if(again.lower() == "y" or again.lower() == "y "):
+    again = str(input("Would you like to perform any other functions? Press y to continue or q to quit: "))
+    if(again.lower() == "y"):
         choose_calc()
-    elif(again.lower() == "n" or again.lower() == "n "):
+    elif(again.lower() == "q"):
         print("Thanks for coming!")
     else:
-        print(f"Please enter a valid option.")
+        print("Please enter a valid option.")
         go_again()
 
 print("Welcome.")
