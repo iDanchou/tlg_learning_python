04d85e770bbe9f4bc4ea43349a09e153d05d3074
iDanchou <rkirkland18@live.com>
2020-08-05 16:32:35 -0700

    proj update
    Signed-off-by: iDanchou <rkirkland18@live.com>

--- Final_Project/hack_sim.py
+++ Final_Project/hack_sim.py
@@ -1,11 +1,11 @@
-import help_function
-import virus_function
+#import help_function
+#import virus_function
 
 login_att = 0  #  VALUE OF LOGIN ATTEMPTS THAT WILL INCREMENT WITH FAILED ATTEMPTS 
 password = "H@CK3R" #  SETTING PASSWORD THAT USER WILL NEED TO FIND
 commands = {"ip addr":"127.0.4.21"}  #  WORKING ON USING DICT FOR CERTAIN VALUES
 name = input("""Welcome. What is your name?
->  """)
+> """)
 
 print(f"""~~~~~~ {name.upper()} ENTERS THE H@CK3R SIMULATOR ~~~~~~ \n
 This will be a test of knowledge and potential.
@@ -21,8 +21,9 @@ while True:  # CREATES A WHILE LOOP THAT FORCES USER TO GUESS CORRECT PASSWORD
         print("""00100010101100010010011
 01001011110001001001001
 01101110100100010010010\n
-Great eyes, You're in. Now lets see about gathering some info.
-Let's check the files and see if there is anything that we can use.\n""")
+Great eyes, You've made it into the test account. 
+Let's check the wireless networks and see who needs to revamp their security.
+If you're not a linux user feel free to use the help command.\n""")
         break
     else:
         print("\n Remember that thing I said about breadcrumbs? Take another look.\n")
